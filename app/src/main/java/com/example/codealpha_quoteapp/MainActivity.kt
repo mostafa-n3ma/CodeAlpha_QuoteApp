10c5f360f7559a2cadd0f4c47cf17be08eca039c
mostafa-n3ma <77761759+mostafa-n3ma@users.noreply.github.com>
2024-02-13 23:29:19 +0300

    feat: hiding the status bar

--- app/src/main/java/com/example/codealpha_quoteapp/MainActivity.kt
+++ app/src/main/java/com/example/codealpha_quoteapp/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.Observer
 import com.example.codealpha_quoteapp.presentation.QuoteViewModel
 import com.example.codealpha_quoteapp.presentation.navigtion.NavigationBarComposable
@@ -26,6 +27,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window,false)
         setContent {
             CodeAlpha_QuoteAppTheme {
                 // A surface container using the 'background' color from the theme
